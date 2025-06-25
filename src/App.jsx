b06b471aab351dc0fcb17763d2461cdf41a03d2a
Steve Lee 李孟儒 <xqopxcat@gmail.com>
2025-06-25 10:02:59 +0800

    fix annotation bug

--- src/App.jsx
+++ src/App.jsx
@@ -98,6 +98,7 @@ function App() {
             setFrameNumber,
             setCurrentFrameData,
             mixerRef,
+            frameRef,
             isPausedRef,
             speedRef,
             selectedJointRef,
